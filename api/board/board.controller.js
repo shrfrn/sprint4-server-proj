b63f1032a7cea3dc6bde78845d147dd26ef4a53f
Avior Kahalani <59415129+AviorKahalani@users.noreply.github.com>
2021-07-20 16:12:10 +0300

    Added settimeout to see loader

--- api/board/board.controller.js
+++ api/board/board.controller.js
@@ -1,26 +1,28 @@
-const boardService = require('./board.service')
-const socketService = require('../../services/socket.service')
-const logger = require('../../services/logger.service')
+const boardService = require('./board.service');
+const socketService = require('../../services/socket.service');
+const logger = require('../../services/logger.service');
 
 async function getBoard(req, res) {
     try {
-        const board = await boardService.getById(req.params.id)
-        res.send(board)
+        const board = await boardService.getById(req.params.id);
+        setTimeout(() => {
+            res.send(board);
+        }, 700);
     } catch (err) {
-        logger.error('Failed to get board', err)
-        res.status(500).send({ err: 'Failed to get board' })
+        logger.error('Failed to get board', err);
+        res.status(500).send({ err: 'Failed to get board' });
     }
 }
 
 async function getBoards(req, res) {
     console.log('in get boards');
     try {
-        const filterBy = (req.query.filterBy) ? JSON.parse(req.query?.filterBy) : {};
-        const boards = await boardService.query(filterBy)
-        res.send(boards)
+        const filterBy = req.query.filterBy ? JSON.parse(req.query?.filterBy) : {};
+        const boards = await boardService.query(filterBy);
+        res.send(boards);
     } catch (err) {
-        logger.error('Failed to get boards', err)
-        res.status(500).send({ err: 'Failed to get boards' })
+        logger.error('Failed to get boards', err);
+        res.status(500).send({ err: 'Failed to get boards' });
     }
 }
 
@@ -28,11 +30,11 @@ async function deleteBoard(req, res) {
     // if(!req.session.user) res.status(401).send({ err: 'Please login' })
     // if(req.session.user.isAdmin === 'false') res.status(403).send({ err: 'not admin' })
     try {
-        await boardService.remove(req.params.id)
-        res.send({ msg: 'Deleted successfully' })
+        await boardService.remove(req.params.id);
+        res.send({ msg: 'Deleted successfully' });
     } catch (err) {
-        logger.error('Failed to delete board', err)
-        res.status(500).send({ err: 'Failed to delete board' })
+        logger.error('Failed to delete board', err);
+        res.status(500).send({ err: 'Failed to delete board' });
     }
 }
 
@@ -40,28 +42,28 @@ async function updateBoard(req, res) {
     // if(!req.session.user) res.status(401).send({ err: 'Please login' })
     // if(req.session.user.isAdmin === 'false') res.status(403).send({ err: 'not admin' })
     try {
-        const board = req.body
+        const board = req.body;
         console.log('--req.body--\n', req.body);
-        const savedBoard = await boardService.update(board)
-        res.send(savedBoard)
+        const savedBoard = await boardService.update(board);
+        res.send(savedBoard);
         // socketService.broadcast({type: 'toy-updated', data: review, to:savedToy._id})
     } catch (err) {
-        logger.error('Failed to update board', err)
-        res.status(500).send({ err: 'Failed to update board' })
+        logger.error('Failed to update board', err);
+        res.status(500).send({ err: 'Failed to update board' });
     }
 }
 
 async function addBoard(req, res) {
     try {
-        const board = req.body
+        const board = req.body;
         console.log('--req.body--\n', req.body);
-        board.createdBy = req.session.user
-        const savedBoard = await boardService.add(board)
-        res.send(savedBoard)
+        board.createdBy = req.session.user;
+        const savedBoard = await boardService.add(board);
+        res.send(savedBoard);
         // socketService.broadcast({type: 'toy-updated', data: review, to:savedToy._id})
     } catch (err) {
-        logger.error('Failed to update board', err)
-        res.status(500).send({ err: 'Failed to update board' })
+        logger.error('Failed to update board', err);
+        res.status(500).send({ err: 'Failed to update board' });
     }
 }
 
@@ -71,4 +73,4 @@ module.exports = {
     deleteBoard,
     addBoard,
     updateBoard,
-}
+};
