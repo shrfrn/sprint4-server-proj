981e9fc49af4470f2c66f44c3330b691b776be32
Avior Kahalani <59415129+AviorKahalani@users.noreply.github.com>
2021-07-22 22:37:13 +0300

    Changes to server, minor

--- services/socket.service.js
+++ services/socket.service.js
@@ -1,96 +1,88 @@
-
-
 const asyncLocalStorage = require('./als.service');
 const logger = require('./logger.service');
 
-var gIo = null
-var gSocketBySessionIdMap = {}
+var gIo = null;
+var gSocketBySessionIdMap = {};
 
 function connectSockets(http, session) {
     gIo = require('socket.io')(http);
 
     const sharedSession = require('express-socket.io-session');
 
-    gIo.use(sharedSession(session, { autoSave: true }))
+    gIo.use(sharedSession(session, { autoSave: true }));
 
-    gIo.on('connection', socket => {
-        console.log('New socket - socket.handshake.sessionID', socket.handshake.sessionID)
-        gSocketBySessionIdMap[socket.handshake.sessionID] = socket
+    gIo.on('connection', (socket) => {
+        console.log('New socket - socket.handshake.sessionID', socket.handshake.sessionID);
+        gSocketBySessionIdMap[socket.handshake.sessionID] = socket;
         console.log();
-        socket.on('disconnect', socket => {
-            console.log('Someone disconnected')
+        socket.on('disconnect', (socket) => {
+            console.log('Someone disconnected');
             if (socket.handshake) {
-                gSocketBySessionIdMap[socket.handshake.sessionID] = null
+                gSocketBySessionIdMap[socket.handshake.sessionID] = null;
             }
-        })
-        socket.on('chat topic', topic => {
+        });
+        socket.on('chat topic', (topic) => {
             if (socket.myTopic === topic) return;
             if (socket.myTopic) {
-                socket.leave(socket.myTopic)
+                socket.leave(socket.myTopic);
             }
-            socket.join(topic)
+            socket.join(topic);
             // logger.debug('Session ID is', socket.handshake.sessionID)
-            socket.myTopic = topic
-        })
-        socket.on('chat newMsg', msg => {
+            socket.myTopic = topic;
+        });
+        socket.on('chat newMsg', (msg) => {
             // emits to all sockets:
             // gIo.emit('chat addMsg', msg)
             // emits only to sockets in the same room
-            gIo.to(socket.myTopic).emit('chat addMsg', msg)
-        })
-        socket.on('user-watch', userId => {
-            socket.join(userId)
-        })
-        socket.on('in-board', boardId => {
+            gIo.to(socket.myTopic).emit('chat addMsg', msg);
+        });
+        socket.on('user-watch', (userId) => {
+            socket.join(userId);
+        });
+        socket.on('in-board', (boardId) => {
             if (socket.boardId === boardId) return;
             if (socket.boardId) {
-                socket.leave(socket.boardId)
+                socket.leave(socket.boardId);
             }
-            socket.join(boardId)
+            socket.join(boardId);
             console.log('joind board:', boardId);
             // logger.debug('Session ID is', socket.handshake.sessionID)
-            socket.boardId = boardId
-        })
-        socket.on('left-board', boardId => {
-            socket.leave(socket.boardId)
-            delete socket.boardId
+            socket.boardId = boardId;
+        });
+        socket.on('left-board', (boardId) => {
+            socket.leave(socket.boardId);
+            delete socket.boardId;
             // logger.debug('Session ID is', socket.handshake.sessionID)
-        })
-        socket.on('board-updated', board => {
-            socket.to(socket.boardId).emit('board-updated', board)
-        })
-
-    })
+        });
+        socket.on('board-updated', (board) => {
+            socket.to(socket.boardId).emit('board-updated', board);
+        });
+    });
 }
 
 function emitToAll({ type, data, room = null }) {
-    if (room) gIo.to(room).emit(type, data)
-    else gIo.emit(type, data)
+    if (room) gIo.to(room).emit(type, data);
+    else gIo.emit(type, data);
 }
 
 // TODO: Need to test emitToUser feature
 function emitToUser({ type, data, userId }) {
-    gIo.to(userId).emit(type, data)
+    gIo.to(userId).emit(type, data);
 }
 
-
-// Send to all sockets BUT not the current socket 
+// Send to all sockets BUT not the current socket
 function broadcast({ type, data, room = null }) {
-    const store = asyncLocalStorage.getStore()
-    const { sessionId } = store
-    if (!sessionId) return logger.debug('Shoudnt happen, no sessionId in asyncLocalStorage store')
-    const excludedSocket = gSocketBySessionIdMap[sessionId]
-    if (!excludedSocket) return logger.debug('Shouldnt happen, No socket in map')
-    if (room) excludedSocket.broadcast.to(room).emit(type, data)
-    else excludedSocket.broadcast.emit(type, data)
+    const store = asyncLocalStorage.getStore();
+    const { sessionId } = store;
+    if (!sessionId) return logger.debug('Shoudnt happen, no sessionId in asyncLocalStorage store');
+    const excludedSocket = gSocketBySessionIdMap[sessionId];
+    if (!excludedSocket) return logger.debug('Shouldnt happen, No socket in map');
+    if (room) excludedSocket.broadcast.to(room).emit(type, data);
+    else excludedSocket.broadcast.emit(type, data);
 }
-
 
 module.exports = {
     connectSockets,
     emitToAll,
     broadcast,
-}
-
-
-
+};
